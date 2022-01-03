f336469416a96e308d57b242d48a78d904306c8b
Douglas Souza <douglassouzabjj@hotmail.com>
2022-01-03 20:24:14 -0300

    Adiciona elementos Header e Intro

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h1>Douglas Souza</h1>
+      <ul>
+        <a href="#home"><li>Home</li></a>
+        <a href="#sobre"><li>Sobre Mim</li></a>
+        <a href="#projetos"><li>Projetos</li></a>
+      </ul>
+    </header>
+  )
+}
+
+export default Header;
---
+++ src/components/Intro.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Intro() {
+  return (
+    <section>
+      <div>
+        <h2>Oi, Eu sou o Douglas</h2>
+        <p>
+          Do interior de São Paulo, curioso, viciado em chocolate e apaixonado por novos desafios. Sigo aprendendo e evoluindo com cada um deles.
+        </p>
+      </div>
+      <div>
+
+      </div>
+    </section>
+  );
+}
+
+export default Intro;
