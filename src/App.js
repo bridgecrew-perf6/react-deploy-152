e5726770ef093da21d6e320a967dfe2576585792
Douglas Souza <douglassouzabjj@hotmail.com>
2022-02-26 11:51:03 -0300

    Adiciona componenet About

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 import Header from './components/Header';
-import Intro from './components/Intro';
+import About from './components/About';
 
 function App() {
   return (
     <>
       <Header />
-      <Intro />
+      <About />
     </>
   );
 }
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
 
-function Intro() {
+function About() {
   return (
     <section>
-      <div>
-        <h2>Oi, Eu sou o Douglas</h2>
-        <p>
-          Do interior de São Paulo, curioso, viciado em chocolate e apaixonado por novos desafios. Sigo aprendendo e evoluindo com cada um deles.
-        </p>
-        <p>
-          Whatever
-        </p>
-      </div>
-      <div>
-
-      </div>
+      <h2>Oi, Eu sou o Douglas</h2>
+      <p>
+        Tenho 27 anos e moro em Santa Isabel - SP.
+      </p>
+      <p>
+        Comecei a estudar programação no início de 2020, depois de passar quase dez anos trabalhando como professor de inglês. Um amigo me incentivou a estudar programação e, depois de muito relutar, dei meus primeiros passos com a linguagem Java. Não parei mais.
+      </p>
+      <p>
+        Hoje sou estudante de desenvolvimento web na Trybe e já desenvolvi projetos com diversas tecnologias. Entre elas: HTML, CSS, JavaScript, Jest, React, Redux, Node.js, TypeScript e muitas outras.
+      </p>
+      <p>
+        Meu próximo objetivo é conseguir meu primeiro emprego como desenvolvedor e seguir aprendendo.
+      </p>
     </section>
   );
 }
 
-export default Intro;
+export default About;
